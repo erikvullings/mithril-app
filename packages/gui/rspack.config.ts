36f9845516ca6c59fc267ad97418096a61a4debf
Erik Vullings <erik.vullings@gmail.com>
2025-07-15 13:46:22 +0200

    Update rspack.config.ts

--- packages/gui/rspack.config.ts
+++ packages/gui/rspack.config.ts
@@ -32,6 +32,7 @@ const configuration: Configuration = {
     asyncWebAssembly: true,
   },
   mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
   entry: {
     main: './src/app.ts',
   },
